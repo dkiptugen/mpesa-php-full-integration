a868ae9016657e83db2c07d00ebd855a44478537
caydee <caydee2010@aol.com>
2018-01-12 23:36:10 +0300

    base mpesa

---
+++ application/config/mpesa.php
@@ -0,0 +1,46 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$config['ConsumerKey']			=	'2SEPZfDFlcBh5pvfQofxcei72Igflotd';
+$config['ConsumerSecret']		=	'oDqu2qEYv9mMJMr8';
+$config["token_link"]			=	'https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials';
+$config["certpass"]				=	'caydeesoft123';
+
+//Mpesa Checkout
+$config['checkout_processlink']	=	'https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest'; 
+$config['checkout_querylink']	=	'https://sandbox.safaricom.co.ke/mpesa/stkpushquery/v1/query';
+$config['checkout_shortcode']	=	'';
+$config['checkout_passkey']		=	'';
+$config['checkout_callbackurl']	=	'';
+
+// Mpesa Reversal
+$config["reversal_link"]		=	'https://sandbox.safaricom.co.ke/mpesa/reversal/v1/request';
+$config["reversal_resultUrl"]	=	'';
+$config["reversal_timeoutURL"]	=	'';
+
+// Mpesa Balance
+$config["balance_link"]			=	'https://sandbox.safaricom.co.ke/mpesa/accountbalance/v1/query';
+$config["balance_timeoutUrl"]	=	'';
+$config["balance_resultUrl"]	=	'';
+
+// Mpesa C2B 
+$config["c2b_regiterUrl"]		=  	'https://sandbox.safaricom.co.ke/mpesa/c2b/v1/registerurl';
+$config["c2b_transactionUrl"]	=	'https://sandbox.safaricom.co.ke/mpesa/c2b/v1/simulate';
+$config["c2b_confirmationURL"]	= 	'';
+$config["c2b_validationURL"]	= 	'';
+$config["c2b_shortcode"]		=	'';
+
+// Mpesa Transaction Status
+$config["transtat_link"]		=	'https://sandbox.safaricom.co.ke/mpesa/transactionstatus/v1/query';
+$config["transtat_resultURL"]	=	'';
+$config["transtat_timeoutURL"]	=	'';
+
+// Mpesa B2B
+$config["b2b_link"]				= 	'https://sandbox.safaricom.co.ke/mpesa/b2b/v1/paymentrequest';
+$config["b2b_timeoutURL"]		=	'';
+$config["b2b_resultURL"]		=	'';
+
+// Mpesa B2C
+$config["b2c_link"]				=	'https://sandbox.safaricom.co.ke/mpesa/b2c/v1/paymentrequest';
+$config["b2c_timeoutURL"]		=	'';
+$config["b22_resultURL"]		=	'';
---
+++ application/libraries/Mpesa.php
@@ -0,0 +1,269 @@
+<?php
+class Mpesa
+	{
+		public $mpesa;
+		protected $ci;
+		public function __construct()
+			{
+				$this->ci 	 =	& get_instance();
+				$this->ci->config->load('mpesa',TRUE,TRUE);
+				$this->mpesa =	(object)$this->ci->config->config["mpesa"];				
+			}
+		public function generatetoken()
+			{				
+				$url 	= 	$this->mpesa->token_link;
+				$curl 	= 	curl_init();
+				curl_setopt($curl, CURLOPT_URL, $url);
+				$credentials = base64_encode($this->mpesa->ConsumerKey.':'.$this->mpesa->ConsumerSecret);
+				curl_setopt($curl, CURLOPT_HTTPHEADER, array('Authorization: Basic '.$credentials)); 
+				curl_setopt($curl, CURLOPT_HEADER, false);
+				curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+				curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+				$curl_response = curl_exec($curl);
+				return json_decode($curl_response);				
+			}
+		public function cert()
+			{
+				$publicKey = APPPATH."cert/cert.cer";			
+				$plaintext = $this->mpesa->certpass;
+				openssl_public_encrypt($plaintext, $encrypted, $publicKey, OPENSSL_PKCS1_PADDING);
+				return base64_encode($encrypted);
+			}
+		public function getIdentifier($type)
+			{
+				switch($type)
+					{
+						case "MSISDN":
+						        $x = 1;
+						        break;
+						case "Till Number":
+								$x = 2;
+								break;
+						case "Shortcode":
+								$x = 4;
+								break;
+					}
+				return $x;
+			}
+		public function checkout($msisdn,$amount,$ref,$desc)
+			{
+				$url 	= $this->mpesa->checkout_processlink;
+				$curl 	= curl_init();
+				curl_setopt($curl, CURLOPT_URL, $url);
+				curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type:application/json','Authorization:Bearer '.$this->generatetoken()));
+                $timestamp 	=	date('YmdHis');
+                $password 	=	base64_encode($this->mpesa->checkout_shortcode.$this->mpesa->checkout_passkey.$timestamp);
+				$curl_post_data = array(				  
+										  	'BusinessShortCode' 	=> $this->mpesa->checkout_shortcode,
+										  	'Password' 				=> $password,
+										  	'Timestamp' 			=> $timestamp,
+										  	'TransactionType' 		=> 'CustomerPayBillOnline',
+										  	'Amount' 				=> $amount,
+										  	'PartyA' 				=> $msisdn,
+										  	'PartyB' 				=> $this->mpesa->checkout_shortcode,
+										  	'PhoneNumber' 			=> $msisdn,
+										  	'CallBackURL' 			=> 'https://ip_address:port/callback',
+										  	'AccountReference' 		=> $ref,
+										  	'TransactionDesc' 		=> $desc
+										);
+				$data_string 	= 	json_encode($curl_post_data);
+				curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+				curl_setopt($curl, CURLOPT_POST, true);
+				curl_setopt($curl, CURLOPT_POSTFIELDS, $data_string);
+				$curl_response 	= 	curl_exec($curl);
+				return $curl_response;
+			}
+		public function checkout_query($CheckoutRequestID)
+			{
+				$url 	= $this->mpesa->checkout_querylink;
+				$curl 	= curl_init();
+				curl_setopt($curl, CURLOPT_URL, $url);
+				curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type:application/json','Authorization:Bearer '.$this->generatetoken())); //setting custom header
+                $timestamp 		=	date('YmdHis');
+                $password 		=	base64_encode($this->mpesa->checkout_shortcode.$this->mpesa->checkout_passkey.$timestamp);
+				$curl_post_data = array(
+										  	'BusinessShortCode' 	=> $this->mpesa->checkout_shortcode,
+										  	'Password' 				=> $password,
+										  	'Timestamp'				=> $timestamp,
+										  	'CheckoutRequestID' 	=> $CheckoutRequestID
+										);
+				$data_string 	= json_encode($curl_post_data);
+				curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+				curl_setopt($curl, CURLOPT_POST, true);
+				curl_setopt($curl, CURLOPT_POSTFIELDS, $data_string);
+				$curl_response = curl_exec($curl);
+				return $curl_response;
+			}
+		public function reversal($Initiator,$cred,$TransID,$amount,$receiver,$remarks,$receiverType,$ocassion)
+			{
+				$url 	= $this->mpesa->reversal_link;
+				$curl 	= curl_init();
+				curl_setopt($curl, CURLOPT_URL, $url);
+				curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type:application/json','Authorization:Bearer '.$this->generatetoken()));
+				$curl_post_data = array(
+										  	'Initiator' 				=> $Initiator,
+										  	'SecurityCredential' 		=> $cred,
+										  	'CommandID' 				=> 'TransactionReversal',
+										  	'TransactionID' 			=> $TransID,
+										  	'Amount' 					=> $amount,
+										  	'ReceiverParty' 			=> $receiver,
+										  	'RecieverIdentifierType' 	=> $this->getIdentifier($receiverType),
+										  	'ResultURL' 				=> $this->mpesa->reversal_resultUrl,
+										  	'QueueTimeOutURL' 		=> $this->mpesa->reversal_timeoutURL,
+										  	'Remarks' 				=> $remarks,
+										  	'Occasion' 				=> $ocassion
+										);
+
+				$data_string = json_encode($curl_post_data);
+				curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+				curl_setopt($curl, CURLOPT_POST, true);
+				curl_setopt($curl, CURLOPT_POSTFIELDS, $data_string);
+				$curl_response = curl_exec($curl);
+				return $curl_response;
+			}
+		public function accountbalance($Initiator,$cred,$shortCode,$IdentifierType,$remark)
+			{
+				$url 	= $this->mpesa->balance_link;
+				$curl 	= curl_init();
+				curl_setopt($curl, CURLOPT_URL, $url);
+				curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type:application/json','Authorization:Bearer '.$this->generatetoken())); 
+				$curl_post_data = array(
+										   	'Initiator' 			=> $Initiator,
+										  	'SecurityCredential' 	=> $cred,
+										  	'CommandID' 			=> 'AccountBalance',
+										  	'PartyA' 				=> $shortcode,
+										  	'IdentifierType' 		=> $this->getIdentifier($IdentifierType),
+										  	'Remarks' 				=> $remark,
+										  	'QueueTimeOutURL' 		=> $this->mpesa->balance_timeoutUrl,
+										  	'ResultURL' 			=> $this->mpesa->balance_resultUrl
+										);
+				$data_string = json_encode($curl_post_data);
+				curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+				curl_setopt($curl, CURLOPT_POST, true);
+				curl_setopt($curl, CURLOPT_POSTFIELDS, $data_string);
+				$curl_response = curl_exec($curl);
+				return $curl_response;
+			}
+		public function C2B_REGISTER($status='Completed')
+			{
+				$url 	= 	$this->mpesa->c2b_regiterUrl;
+				$curl 	= 	curl_init();
+				curl_setopt($curl, CURLOPT_URL, $url);
+				curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type:application/json','Authorization:Bearer '.$this->generatetoken())); 
+				$curl_post_data = array(				  
+										  	'ShortCode' 		=> $this->mpesa->c2b_shortcode,
+										  	'ResponseType' 		=> $status,
+										  	'ConfirmationURL' 	=> $this->mpesa->c2b_confirmationUrl,
+										  	'ValidationURL' 	=> $this->mpesa->c2b_validationUrl
+										);
+				$data_string = json_encode($curl_post_data);
+				curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+				curl_setopt($curl, CURLOPT_POST, true);
+				curl_setopt($curl, CURLOPT_POSTFIELDS, $data_string);
+				$curl_response = curl_exec($curl);
+				return $curl_response;
+			}
+		public function C2B($amount,$msisdn,$ref)
+			{				
+				$url 	= $this->mpesa->c2b_transactionUrl;
+				$curl 	= curl_init();
+				curl_setopt($curl, CURLOPT_URL, $url);
+				curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type:application/json','Authorization:Bearer '.$this->generatetoken())); 
+				$curl_post_data = array(				  
+										    "ShortCode"		=>	$this->mpesa->c2b_shortcode,
+										    "CommandID"		=>	"CustomerPayBillOnline",
+										    "Amount"		=> 	$amount,
+										    "Msisdn"		=>	$msisdn,
+										    "BillRefNumber"	=>	$ref
+										);
+				$data_string = json_encode($curl_post_data);
+				curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+				curl_setopt($curl, CURLOPT_POST, true);
+				curl_setopt($curl, CURLOPT_POSTFIELDS, $data_string);
+				$curl_response = curl_exec($curl);
+				return $curl_response;
+			}
+		public function B2B($initiator,$cred,$CommandID,$SenderIdentifier,$RecieverIdentifier,$amount,$sender,$reciever,$accountref,$remarks)
+			{
+				$url 	= 	$this->mpesa->b2b_link;
+				$curl 	= 	curl_init();
+				curl_setopt($curl, CURLOPT_URL, $url);
+				curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type:application/json','Authorization:Bearer '.$this->generatetoken())); 
+				$curl_post_data = array(
+										  	'Initiator' 				=> $initiator,
+										  	'SecurityCredential' 		=> $cred,
+										  	'CommandID' 				=> $CommandID,
+										  	'SenderIdentifierType' 		=> $this->getIdentifier($SenderIdentifier),
+										  	'RecieverIdentifierType' 	=> $this->getIdentifier($RecieverIdentifier),
+										  	'Amount' 					=> $amount,
+										  	'PartyA' 					=> $sender,
+										  	'PartyB' 					=> $reciever,
+										  	'AccountReference' 			=> $accountref,
+										  	'Remarks' 					=> $remarks,
+										  	'QueueTimeOutURL' 			=> $this->mpesa->b2b_timeoutURL,
+										  	'ResultURL' 				=> $this->mpesa->b2b_resultURL
+										);
+				$data_string = json_encode($curl_post_data);
+				curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+				curl_setopt($curl, CURLOPT_POST, true);
+				curl_setopt($curl, CURLOPT_POSTFIELDS, $data_string);
+				$curl_response = curl_exec($curl);
+				return $curl_response;
+			}
+		public function B2C($initiator,$cred,$CommandID,$amount,$sender,$reciever,$remarks,$ocassion)
+			{
+				$url = $this->mpesa->b2c_link;
+				$curl = curl_init();
+				curl_setopt($curl, CURLOPT_URL, $url);
+				curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type:application/json','Authorization:Bearer '.$this->generatetoken())); 
+				$curl_post_data = array(
+										  	'InitiatorName' 		=> 	$initiator,
+										  	'SecurityCredential' 	=> 	$cred,
+										  	'CommandID' 			=> 	$CommandID,
+										  	'Amount' 				=> 	$amount,
+										  	'PartyA' 				=> 	$sender,
+										  	'PartyB' 				=> 	$reciever,
+										  	'Remarks' 				=> 	$remarks,
+										  	'QueueTimeOutURL' 		=> 	$this->mpesa->b2c_timeoutURL,
+										  	'ResultURL' 			=> 	$this->mpesa->b2c_resultURL,
+										  	'Occasion' 				=> 	$ocassion
+										);
+
+				$data_string = json_encode($curl_post_data);
+
+				curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+				curl_setopt($curl, CURLOPT_POST, true);
+				curl_setopt($curl, CURLOPT_POSTFIELDS, $data_string);
+
+				$curl_response = curl_exec($curl);
+				print_r($curl_response);
+
+				echo $curl_response;
+			}
+		public function transactionstatus($initiator,$cred,$transID,$msisdn,$identifier,$remarks,$ocassion)
+			{
+				$url 	=	$this->mpesa->transtat_link;
+				$curl 	= 	curl_init();
+				curl_setopt($curl, CURLOPT_URL, $url);
+				curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type:application/json','Authorization:Bearer '.$this->generatetoken()));
+				$curl_post_data = array(
+										  	'Initiator' 			=> $initiator,
+										  	'SecurityCredential' 	=> $cred,
+										  	'CommandID' 			=> 'TransactionStatusQuery',
+										  	'TransactionID' 		=> $transID,
+										  	'PartyA' 				=> $msisdn,
+										  	'IdentifierType' 		=> $this->getIdentifier($identifier),
+										  	'ResultURL' 			=> $this->mpesa->transtat_resultURL,
+										  	'QueueTimeOutURL' 		=> $this->mpesa->transtat_timeoutURL,
+										  	'Remarks' 				=> $remarks,
+										  	'Occasion' 				=> $ocassion
+										);
+				$data_string = json_encode($curl_post_data);
+				curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+				curl_setopt($curl, CURLOPT_POST, true);
+				curl_setopt($curl, CURLOPT_POSTFIELDS, $data_string);
+				$curl_response = curl_exec($curl);
+				return $curl_response;
+			}
+		
+	}
