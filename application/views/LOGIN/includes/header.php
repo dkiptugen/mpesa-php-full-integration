4503cd861a911987034ac9af44605aa8216cf104
caydee <caydee2010@aol.com>
2018-01-19 16:42:23 +0300

    login

--- application/views/LOGIN/includes/header.php
+++ application/views/LOGIN/includes/header.php
@@ -3,5 +3,5 @@
  * Created by PhpStorm.
  * User: dkiptugen
  * Date: 1/19/18
- * Time: 1:26 PM
+ * Time: 1:40 PM
  */
---
+++ application/views/LOGIN/modules/login.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dkiptugen
+ * Date: 1/19/18
+ * Time: 1:26 PM
+ */
+echo time();
